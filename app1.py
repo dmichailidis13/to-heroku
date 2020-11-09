25f7d5a36ff246eb7b71b8214964a8282142882c
Dimitrios Michailidis <dimitriosmichailidis@dimitrioss-mbp.mynet>
2020-11-09 16:59:23 +0000

    Added postgre sql

--- app1.py
+++ app1.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask #request
 from flask_restful import Api #reqparse
 from flask_jwt import JWT #jwt_required
@@ -9,7 +11,7 @@ from resources.store import Store, StoreList
 #from db import db
 
 app1 = Flask(__name__)
-app1.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app1.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app1.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app1.secret_key = 'dimitris'
 api = Api(app1)
